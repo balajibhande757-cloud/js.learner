815ad66535368f55e0a538eef7f8d7c4f20773e7
balajibhande757-cloud <balajibhande757@gmail.com>
2026-05-27 11:56:43 +0000

    operations

---
+++ Basics/05_operations.js
@@ -0,0 +1,55 @@
+// let value = 9 
+// let  Negvalue = -value
+// console.log(Negvalue)
+
+// console.log(2**3)
+// console.log(2+3)
+// console.log(2-3)
+// console.log(2*3)
+// console.log(2/3)
+
+// let str1 = "hello"
+// let str2 = " mahesh"
+// let str3 = str1 + str2
+// console.log(str3);
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log(+true);
+// console.log(+"");
+// let num1,num2,num3
+// num1=num2=num3=2+2
+
+// let gameCounter = 100
+// gameCounter++;
+// console.log(gameCounter);
+
+
+// prefix
+let p = 5;
+
+let q = ++p;
+
+console.log(p); // 6
+console.log(q); // 6
+
+//postfix
+let x = 5;
+
+let y = x++;
+
+console.log(x); // 6
+console.log(y); // 6
+
+
+/*
+
+| Type              | Syntax | Action                   |
+| ----------------- | ------ | ------------------------ |
+| Prefix Increment  | `++x`  | Increase first, then use |
+| Postfix Increment | `x++`  | Use first, then increase |
+| Prefix Decrement  | `--x`  | Decrease first, then use |
+| Postfix Decrement | `x--`  | Use first, then decrease |
+
+*/
