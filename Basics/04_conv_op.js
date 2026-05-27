71d63d28640643528c3090c24310308424567f30
balajibhande757-cloud <balajibhande757@gmail.com>
2026-05-27 11:27:17 +0000

    conversion

---
+++ Basics/04_conv_op.js
@@ -0,0 +1,19 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueINNumber = Number(score)
+console.log(typeof valueINNumber);
+console.log(typeof (valueINNumber));
+console.log( valueINNumber);
+
+
+let islogedIn = "lala"                                     // empty string is give false
+let BooleanISlogedin =Boolean(islogedIn)
+console.log(BooleanISlogedin);
+
+let someNo =45
+let stringNo = String(someNo)
+console.log( stringNo)
+console.log(typeof stringNo)
